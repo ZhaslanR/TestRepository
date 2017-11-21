5176226610b7a4e30664c7e9e1a24659b47a0dc6
ZhaslanR <zhaslan_12@bk.ru>
2017-11-21 16:55:47 +0600

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Y;
+            Console.Write("Ведите число X для того что-бы вычеслить число Y");
+            Console.Read();
+
+            
+        }
+    }
+}
